050d8ef765c98631cd6011917f118e672b91a30e
Cristian Fabricio Rodriguez Romero <fabricio.rodriguez@youse.com.co>
2017-10-06 07:17:05 -0500

    add documents controller and view

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,33 @@
+class DocumentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+    user
+  end
+
+  def update
+    if user.update_without_password(user_params)
+      validator = ImagesValidator.new
+      document = validator.valid_document? document_front_side: user.document_front_side.path, document_back_side: user.document_back_side.path
+      validation_for_continue = document[:valid?]
+
+      if validation_for_continue
+        user.request_logs.create(
+          { request_type: :document, request_content: document[:data] }
+        )
+      end
+
+      redirect_to :edit_users, notice: "Validaciones #{validation_for_continue ? ';)' : ':('}"
+    end
+  end
+
+  private
+
+  def user
+    @user ||= current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:document_front_side, :document_back_side)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -37,9 +37,6 @@ ActiveRecord::Schema.define(version: 20171006114428) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
@@ -57,7 +54,6 @@ ActiveRecord::Schema.define(version: 20171006114428) do
     t.text "image"
     t.jsonb "journey"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "credentials", "users"
