1354d6ae99f6630e20cbe17c34653a5df4233e6e
Cristian Fabricio Rodriguez Romero <fabricio.rodriguez@youse.com.co>
2017-10-06 11:36:28 -0500

    visualization of states of the steps of validation

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,15 @@
 class ResultsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+     @steps = user.journey
+     fb_evaluator = FacebookEvaluator.new(user)
+     @percentage_fb = fb_evaluator.calculate_score
+  end
+
+  private
+
+  def user
+    @user ||= current_user
   end
 end
