6f17f859654a7003c92c50add2e0bd732c59ded8
Leonardo Celis <leonardocelis112@gmail.com>
2017-10-06 02:56:41 -0500

    save request logs

--- app/models/linkedin_evaluator.rb
+++ app/models/linkedin_evaluator.rb
@@ -1,8 +1,11 @@
 class LinkedinEvaluator
   attr_accessor :client
+  attr_reader :user
+
   BASIC_PROFILE_FIELDS = ['headline', 'positions', 'first_name', 'last_name', 'industry']
 
   def initialize(user)
+    @user = user
     credential = user.credentials.find_by_provider('linkedin')
     tokens = credential.token.split(' ')
     @client = LinkedIn::Client.new(ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'])
@@ -11,6 +14,13 @@ class LinkedinEvaluator
 
   def get_profile_fields
     client.profile(fields: BASIC_PROFILE_FIELDS).as_json
+  end
+
+  def save
+    user.request_logs.create(
+      request_type: :linkedin,
+      request_content: get_profile_fields
+    )
   end
 
 end
